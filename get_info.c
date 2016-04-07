dfed3266204f65ce1bcbd62d42bd79ff26fc15e3
Thibault FRANCOIS <thifranc@e3r7p3.42.fr>
2016-04-07 17:55:51 +0200

    improved

--- get_info.c
+++ get_info.c
@@ -6,25 +6,29 @@
 /*   By: thifranc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/07 17:29:31 by thifranc          #+#    #+#             */
-/*   Updated: 2016/04/07 17:46:05 by thifranc         ###   ########.fr       */
+/*   Updated: 2016/04/07 17:53:39 by thifranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <time.h>
 #include <pwd.h>
+#include <grp.h>
 
 int		main(int ac, char **av)
 {
 	struct stat		lol;
 	struct passwd	*mdr;
+	struct group	*xd;
 
 	if (ac == 2)
 	{
 		lstat(av[1], &lol);
 		mdr = getpwuid(lol.st_uid);
-		printf("%s\n", mdr->pw_name);
+		xd = getgrgid(lol.st_gid);
+		printf("%s == auteur || %s == group || %s == time\n", mdr->pw_name, xd->gr_name);
 	}
 	return (0);
 }
