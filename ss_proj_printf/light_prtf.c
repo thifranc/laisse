d77fa3c960205cfe4f637f3218247ccc571784d5
Thibault FRANCOIS <thifranc@e3r6p4.42.fr>
2016-04-13 15:42:43 +0200

    dvlpmt brouillom

--- ss_proj_printf/light_prtf.c
+++ ss_proj_printf/light_prtf.c
@@ -6,7 +6,7 @@
 /*   By: thifranc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/13 11:50:27 by thifranc          #+#    #+#             */
-/*   Updated: 2016/04/13 13:02:22 by thifranc         ###   ########.fr       */
+/*   Updated: 2016/04/13 15:24:42 by thifranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,18 +32,75 @@ char	*get_till_opt(char **str)
 	return (out);
 }
 
-char	*get_opt_and_arg(char **str)
+char	*get_opt_and_arg(char **s, va_list va)
 {
-	int		opt;
+	int				opt;
+	int				max;
+	long long int	d_arg;
+	char			*s_arg
 
 	opt = 0;
-	(*str)++;
+	max = 0;
+	(*s)++;
 	if (**s == '-')
 	{
-		opt = | 128;
+		opt = 1;
 		(*s)++;
 	}
+	if (**s == '*')
+	{
+		max = va_arg(va, int);
+		(*s)++;
+	}
+	if (**s == 's')
+		return (do_string(opt, max, va));
+	else
+		return (do_nb(opt, max, va));
+}
 
+char	*do_nb(int opt, int max)
+{
+	char	*out;
+	int		arg;
+
+	arg = va_arg(va, int);
+	len = ft_nb_len_base(arg, 10);
+	if (!(out = malloc(sizeof * (max > len ? max : len) + 1)))
+		return (NULL);
+	out[max > len ? max : len] = '\0';
+
+}
+
+char	*do_string(int opt, int max)
+{
+	char	*out;
+	char	*arg;
+	int		i;
+	int		k;
+
+	i = 0;
+	arg = va_arg(va, const char *);
+	len = ft_strlen(arg);
+	if (!(out = malloc(sizeof * (max > len ? max : len) + 1)))
+		return (NULL);
+	while (max > len && !opt)
+	{
+		out[i++] = ' ';
+		max--;
+	}
+	k = 0;
+	while (arg[k])
+	{
+		out[i + k] = arg[k];
+		k++;
+	}
+	while (max > len && opt)
+	{
+		out[k++] = ' ';
+		max--;
+	}
+	out[k + i] = '\0';
+	return (out);
 }
 
 void	print_it(char *str, ...)
