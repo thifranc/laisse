478e6439bd906663278a338442ad12a55746ecd1
Thibault FRANCOIS <thifranc@e3r3p1.42.fr>
2016-04-17 11:10:14 +0200

    resol pb if day is one char only

--- get_date.c
+++ get_date.c
@@ -6,7 +6,7 @@
 /*   By: thifranc <thifranc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/17 09:34:40 by thifranc          #+#    #+#             */
-/*   Updated: 2016/04/17 10:46:45 by thifranc         ###   ########.fr       */
+/*   Updated: 2016/04/17 11:09:31 by thifranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*get_date(time_t t)
 	out = ctime(&t);
 	splitted = ft_strsplit(out, ' ');
 	splitted[3][5] = '\0';
-	out = print_it("%s %s %s ", splitted[1], splitted[2], splitted[3]);
+	out = print_it("%s %*s %s ", splitted[1], 2, splitted[2], splitted[3]);
 	ft_memdel((void*)&(*splitted));
 	return (out);
 }
