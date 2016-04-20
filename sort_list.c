d079aa2548fcf2302c20e9778041b95df13b800a
Thibault FRANCOIS <thifranc@e3r5p1.42.fr>
2016-04-20 16:38:03 +0200

    normage

--- sort_list.c
+++ sort_list.c
@@ -6,7 +6,7 @@
 /*   By: thifranc <thifranc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/14 13:47:12 by thifranc          #+#    #+#             */
-/*   Updated: 2016/04/20 16:34:16 by thifranc         ###   ########.fr       */
+/*   Updated: 2016/04/20 16:37:45 by thifranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,10 +82,7 @@ void	recur_sort(t_list **list, int opt)
 	else if (opt & OPT_C)
 		*list = fusion(a, b, status);
 	else if (opt & OPT_U)
-	{
-		dprintf(1, "TRI MOI CA");
 		*list = fusion(a, b, t_access);
-	}
 	else
 		*list = fusion(a, b, ascii);
 }
